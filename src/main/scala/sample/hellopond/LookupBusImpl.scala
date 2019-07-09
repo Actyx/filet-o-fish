60abd749dbba2bb3007d2b1d9fcc5988175e9c4b
Jan Pustelnik <jan@actyx.io>
2019-07-09 10:33:56 +0200

    A little moving around

---
+++ src/main/scala/sample/hellopond/LookupBusImpl.scala
@@ -0,0 +1,39 @@
+package sample.hellopond
+
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.ActorRef
+import akka.actor.Actor
+import akka.actor.ActorLogging
+import akka.actor.Terminated
+import akka.event.EventBus
+import akka.event.LookupClassification
+
+/**
+ * Publishes the payload of the EventEnvelope when the topic of the
+ * EventEnvelope equals the String specified when subscribing.
+ */
+class LookupBusImpl extends EventBus with LookupClassification {
+    type Event = EventEnvelope
+    type Classifier = String
+    type Subscriber = ActorRef
+  
+    // is used for extracting the classifier from the incoming events
+    override protected def classify(event: Event): Classifier = event.topic
+  
+    // will be invoked for each event for all subscribers which registered themselves
+    // for the event’s classifier
+    override protected def publish(event: Event, subscriber: Subscriber): Unit = {
+      subscriber ! event.payload
+    }
+  
+    // must define a full order over the subscribers, expressed as expected from
+    // `java.lang.Comparable.compare`
+    override protected def compareSubscribers(a: Subscriber, b: Subscriber): Int =
+      a.compareTo(b)
+  
+    // determines the initial size of the index data structure
+    // used internally (i.e. the expected number of different classifiers)
+    override protected def mapSize: Int = 128
+  
+  }
